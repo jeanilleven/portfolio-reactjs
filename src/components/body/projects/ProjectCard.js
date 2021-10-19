b0a2e12c3fa91dd5a6452bcd08696305abd0cffc
Jeanille Ven <jeanillevenabayon30@gmail.com>
2021-10-20 02:24:57 +0800

    project card zoom in up

--- src/components/body/projects/ProjectCard.js
+++ src/components/body/projects/ProjectCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './projectcard.css'
 
 function ProjectCard({project}) {
-  return <div className='project-card' data-aos="fade-up" >
+  return <div className='project-card' data-aos="zoom-in-up" >
     <div className='project-info'>
       <label className='project-title'>{project.title}</label>
       <p className='project-about'>
