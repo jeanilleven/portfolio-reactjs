2417beb31a312440ce772f0c9b19dbf6d01aa04e
Jeanille Ven <jeanillevenabayon30@gmail.com>
2021-10-21 15:01:58 +0800

    added animation to testimony carousel

--- src/components/body/testimonials/Testimonials.js
+++ src/components/body/testimonials/Testimonials.js
@@ -2,8 +2,6 @@ import React, { useEffect} from 'react'
 import Separator from '../../../common/separator/Separator'
 import './testimonials.css'
 import { TestimonialsData } from '../../data/testimonials'
-
-// import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
 import Aos from 'aos'
@@ -22,13 +20,13 @@ function Testimonials() {
       <Separator />
       <label className='section-title'>What do they say about me?</label>
       <div className='testimonials-container'>
-        <Carousel variant="dark" >
+        <Carousel variant="dark" data-aos='fade-up'>
           {data.map((testimony)=>{
             return <Carousel.Item>
               <div className='testimony-details'>
                 <span className='testimony-card'>
                   <p className='testimony-name'>{testimony.name}</p>
-                  <p className='testimony-comment'>{testimony.feedback}</p>
+                  <p className='testimony-comment'>"{testimony.feedback}"</p>
                 </span>
               </div>
             </Carousel.Item>
